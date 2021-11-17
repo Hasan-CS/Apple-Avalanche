b51fe871a0d0f0fb1a157dbae95f857b00df2ffc
Hasan Ahmad <91635325+Hasan-CS@users.noreply.github.com>
2021-11-17 13:15:21 -0800

    Add files via upload

--- apple_game.py
+++ apple_game.py
@@ -16,6 +16,7 @@ appl = trtl.Turtle()
 app = trtl.Turtle()
 ap = trtl.Turtle()
 a = trtl.Turtle()
+also_a = trtl.Turtle()
 
 drawer = trtl.Turtle()
 drawer.hideturtle()
@@ -26,6 +27,31 @@ the_letter_two = rand.choice(letters)
 the_letter_three = rand.choice(letters)
 the_letter_four = rand.choice(letters)
 the_letter_five = rand.choice(letters)
+the_letter_six = rand.choice(letters)
+
+#-----countdown setup-----
+font_setup = ("Arial", 30, "normal")
+timer = 6
+counter_interval = 1000   
+timer_up = False
+
+counter = trtl.Turtle()
+counter.color("white")
+counter.penup()
+counter.goto(-250, -200)
+
+#-----countdown-----------
+def countdown():
+  global timer, timer_up
+  counter.clear()
+  counter.hideturtle()
+  if timer <= 0:
+    counter.write("Time's Up", font=font_setup)
+    timer_up = True
+  else:
+    counter.write("Timer: " + str(timer), font=font_setup)
+    timer -= 1
+    counter.getscreen().ontimer(countdown, counter_interval)
 
 #-----functions-----
 def setup():
@@ -34,12 +60,14 @@ def setup():
   app.penup()
   ap.penup()
   a.penup()
+  also_a.penup()
 
   apple.goto(rand.randint(-150, 150), rand.randint(0, 150))
   appl.goto(rand.randint(-150, 150), rand.randint(0, 150))
   app.goto(rand.randint(-150, 150), rand.randint(0, 150))
   ap.goto(rand.randint(-150, 150), rand.randint(0, 150))
   a.goto(rand.randint(-150, 150), rand.randint(0, 150))
+  also_a.goto(rand.randint(-150, 150), rand.randint(0, 150))
 
 def draw_turtle(active_apple, active_gif):
   active_apple.shape(active_gif)
@@ -51,38 +79,57 @@ def draw_letter(which_letter):
 
 #-----apple functions----
 def apple_fall():
-  drawer.clear()
-  apple.penup()
-  apple.goto(0, -200)
-  apple.hideturtle()
-  draw_letter(the_letter_two)
+  global timer_up
+  if timer_up == False:
+    drawer.clear()
+    apple.penup()
+    apple.goto(0, -200)
+    apple.hideturtle()
+    draw_letter(the_letter_two)
 
 def appl_fall():
-  drawer.clear()
-  appl.penup()
-  appl.goto(0, -200)
-  appl.hideturtle()
-  draw_letter(the_letter_three)
+  global timer_up
+  if timer_up == False:
+    drawer.clear()
+    appl.penup()
+    appl.goto(0, -200)
+    appl.hideturtle()
+    draw_letter(the_letter_three)
 
 def app_fall():
-  drawer.clear()
-  app.penup()
-  app.goto(0, -200)
-  app.hideturtle()
-  draw_letter(the_letter_four)
+  global timer_up
+  if timer_up == False:
+    drawer.clear()
+    app.penup()
+    app.goto(0, -200)
+    app.hideturtle()
+    draw_letter(the_letter_four)
 
 def ap_fall():
-  drawer.clear()
-  ap.penup()
-  ap.goto(0, -200)
-  ap.hideturtle()
-  draw_letter(the_letter_five)
+  global timer_up
+  if timer_up == False:
+    drawer.clear()
+    ap.penup()
+    ap.goto(0, -200)
+    ap.hideturtle()
+    draw_letter(the_letter_five)
 
 def a_fall():
-  drawer.clear()
-  a.penup()
-  a.goto(0, -200)
-  a.hideturtle()
+  global timer_up
+  if timer_up == False:
+    drawer.clear()
+    a.penup()
+    a.goto(0, -200)
+    a.hideturtle()
+    draw_letter(the_letter_six)
+
+def also_a_fall():
+  global timer_up
+  if timer_up == False:
+    drawer.clear()
+    also_a.penup()
+    also_a.goto(0, -200)
+    also_a.hideturtle()
 
 #-----function calls-----
 draw_turtle(apple, apple_image)
@@ -90,8 +137,11 @@ draw_turtle(appl, apple_image)
 draw_turtle(app, apple_image)
 draw_turtle(ap, apple_image)
 draw_turtle(a, apple_image)
+draw_turtle(also_a, apple_image)
 
 setup()
+
+countdown()
 
 draw_letter(the_letter)
 wn.onkeypress(apple_fall, the_letter)
@@ -99,6 +149,7 @@ wn.onkeypress(appl_fall, the_letter_two)
 wn.onkeypress(app_fall, the_letter_three)
 wn.onkeypress(ap_fall, the_letter_four)
 wn.onkeypress(a_fall, the_letter_five)
+wn.onkeypress(also_a_fall, the_letter_six)
 
 wn.listen()
 wn.mainloop()
